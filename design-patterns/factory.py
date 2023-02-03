e67f54a081767a0c02da95e610250e32b5320842
4the4ryushin <aman2001mi@gmail.com>
2023-02-04 00:00:22 +0530

    facotry code complete

--- design-patterns/factory.py
+++ design-patterns/factory.py
@@ -30,7 +30,7 @@ class FrogWorld:
         return "\n\n\t------Frog World------"
 
     def make_character(self):
-        return Frog()
+        return Frog(self.player_name)
 
     def make_obstacle(self):
         return Bug()
@@ -46,6 +46,12 @@ class Wizard:
         print(msg)
 
 class Ork:
+    def __str__(self):
+        return 'an evil ork'
+    def action(self):
+        return 'kills it'
+
+class WizardWorld:
     def __init__(self, name):
         print(self)
         self.player_name = name
@@ -53,14 +59,35 @@ class Ork:
         return "\n\n\t------Frog World------"
 
     def make_character(self):
-        return Wizard()
+        return Wizard(self.player_name)
     def make_obstacle(self):
         return Ork()
 
-class GameEnvironmentt:
+class GameEnvironment:
     def __init__(self, factory):
         self.hero = factory.make_character()
         self.obstacle = factory.make_obstacle()
 
     def play(self):
         self.hero.interact_with(self.obstacle)
+
+def validate_age(name):
+    try:
+        age = int(input(f"welcome {name}. How old are you?"))
+    except ValueError as e:
+        print(f"Age {age} is invalid please try again...")
+        return (False, age)
+    return (True, age)
+
+def main():
+    name = input(f"Welcome. input your name")
+    valid_name = False
+
+    while not valid_name:
+        valid_name, age = validate_age(name)
+    game = FrogWorld if age < 18 else WizardWorld
+    environment = GameEnvironment(game(name))
+    environment.play
+
+if __name__=='__main__':
+    main()
